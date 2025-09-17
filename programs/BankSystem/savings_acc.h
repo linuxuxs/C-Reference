cbf8675d1708019e3f7505d07eb5a75007a0a771
0x4B6973734D653A33 <lessetee.261@gmail.com>
2025-09-17 22:00:05 +0300

    Update savings_acc.h

--- programs/BankSystem/savings_acc.h
+++ programs/BankSystem/savings_acc.h
@@ -1,11 +1,6 @@
 #pragma once
 #include "account.h"
 
-//TODO: after done and it compilers, check if you can use default_balance from the 
-//inherited class
-//It doesnt work, because its private and it cant access it.
-//to fix that, js change default_balance to protected/private in the base class
-
 //This savings account deposit will be incremented by (amount * int_rate / 100)
 
 class Savings_account : public Account {
@@ -19,6 +14,7 @@ protected:
 public:
     Savings_account(std::string name_ = default_name, double balance_ = default_balance, double interest_rate_ = default_interest_rate);
     bool deposit(double amount);
-    //withdraw inherited from account
+    //withdraw method inherited from account
 };
+
 
